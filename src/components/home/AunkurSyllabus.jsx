54db966a9977e139ad0bb3af05738d72e381137e
dev-rakibul-islam <rakibulislam.eb@gmail.com>
2025-07-18 01:54:32 +0600

    Chairman Message Update

--- src/components/home/AunkurSyllabus.jsx
+++ src/components/home/AunkurSyllabus.jsx
@@ -132,7 +132,7 @@ const AunkurSyllabus = () => {
                 <div className="bg-green-100 rounded-2xl items-center shadow p-7 min-h-[260px] flex flex-col w-full h-full">
                   <h3 className="font-bold text-lg mb-2">{item.title}</h3>
                   <span className="inline-flex items-center bg-white text-gray-800 px-3 py-1 rounded font-semibold text-sm mb-3 mt-1 shadow-sm">
-                    <MdStarPurple500 className="mr-1" /> Subjects
+                    <MdStarPurple500 className="mr-1" /> বিষয়সমূহ
                   </span>
                   <ul className="grid grid-cols-2 gap-x-2 pl-4 mb-4 text-gray-700 text-base">
                     {item.subjects.map((sub, i) => (
--- src/components/home/ChairmanMessage.jsx
+++ src/components/home/ChairmanMessage.jsx
@@ -1,99 +1,63 @@
 import React from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/scrollbar";
-
-// Import required modules
-import { FreeMode, Scrollbar, Mousewheel } from "swiper/modules";
+import ChairmanProfile from "../../assets/Chairman.jpg";
 
 const ChairmanMessage = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between gap-6 px-4 py-8 w-10/12 mx-auto">
-      {/* Swiper container */}
-      <div className="w-full md:w-1/2">
-        <h4 className="text-4xl font-bold mb-4 text-center">
-          Chairman's Message
-        </h4>
-        <Swiper
-          direction={"vertical"} // Enable vertical scrolling
-          slidesPerView={"auto"} // Adjust slides dynamically
-          freeMode={true} // Enable free scrolling
-          scrollbar={{
-            draggable: true, // Make scrollbar draggable
-          }}
-          mousewheel={{
-            forceToAxis: true,
-            sensitivity: 1,
-            // Ensure scrolling works vertically
-          }}
-          modules={[FreeMode, Scrollbar, Mousewheel]} // Include required modules
-          className="mySwiper"
-          style={{
-            height: "300px", // Set fixed height for the scrollable container
-            border: "1px solid #ccc", // Add border for styling
-            borderRadius: "8px", // Rounded corners
-            padding: "16px", // Add padding inside the container
-            backgroundColor: "#f9f9f9", // Light background color
-            overflow: "auto", // Enable scrolling
-          }}
-        >
-          <SwiperSlide>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
-              fuga similique tenetur provident tempora illum harum a explicabo
-              corporis odio commodi, quo expedita illo recusandae aliquam culpa
-              sit dolorum repellendus perspiciatis iusto! Aut vel provident,
-              repellendus quibusdam, harum asperiores laudantium maiores
-              commodi, omnis quisquam facilis nobis veniam optio libero tempore.
+    <section className="px-4 py-12 sm:px-6 lg:px-8">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+        <h1 className="text-2xl lg:text-5xl font-semibold text-gray-800 mb-4 text-center">
+          চেয়ারম্যানের বাণী
+        </h1>
+        <div className="bg-white shadow-md rounded-lg p-6 lg:p-6 flex flex-col lg:flex-row items-center lg:items-start">
+          {/* Left side text */}
+          <div className="lg:w-4/5">
+            <p className="text-lg text-gray-700 text-justify leading-relaxed mb-4">
+              সম্মানিত অভিভাবক/শিক্ষকমণ্ডলী
+              <br />
+              আসসালামু আলাইকুম
+              <br />
+              <br />
+              আমরা জাতি হিসেবে বেশ সমৃদ্ধ ও আত্মমর্যাদাশীল। আমাদের কাছে একটি
+              ঐতিহ্যময় ইতিহাস যা আমাদেরকে বিশ্ব দরবারে উচ্চ আসনে সমাসীন করেছে।
+              বিশ্ব পরিমণ্ডলে আমরা পেয়েছি আন্তর্জাতিক মাতৃভাষা দিবসের স্বীকৃতি
+              যা আমাদের জাতিস্বত্তাকে আরো অলংকৃত ও সমুজ্জ্বল করেছে। আমাদের
+              জাতিসত্ত্বা ও স্বকীয়তাকে অক্ষুণ্ণ রাখতে দু-দুবার স্বাধীনতাও লাভ
+              করেছি। কিন্তু বৃটিশ বেনিয়াদের বেঁধে দেওয়া শিক্ষাব্যবস্থা থেকে
+              পারিনি নিজেদের স্বাধীন করতে, দু-একবার চেষ্টা অবশ্য হয়েছিল। কিন্তু
+              অন্ধকার আরো প্রগাঢ় হওয়ায় তা সফলতার মুখ দেখেনি। ফলে জাতিও পায়নি
+              শিক্ষার সঠিক আলো। প্রকৃতপক্ষে শিক্ষার মূল উদ্দেশ্য হল মনুষ্যত্বের
+              পূর্ণ বিকাশ সাধন। কিন্তু আমাদের শিক্ষা ব্যবস্থায় তা হয়েছে
+              উপেক্ষিত। আমাদের শিক্ষা ব্যবস্থা যে স্বপ্নীল মানুষ তৈরির কথা ছিল
+              তা পারেনি। বিশ্ববিদ্যালয়ের সর্বোচ্চ ডিগ্রীধারী অসামান্য মেধার
+              অধিকারীরাও জড়িয়ে যাচ্ছে দুর্নীতির সাথে। আর এ মেধাবীরাই যদি
+              দুর্নীতি করে তাহলে দেশটাকে বাঁচাবে কারা? এসব গাঢ় তমাসাচ্ছন্ন
+              আশাহীনতার মূল কারণ হল অস্বচ্ছ সিলেবাস, বাণিজ্য নির্ভর ও নৈতিকতাহীন
+              শিক্ষা ব্যবস্থা। ক্রমবর্ধমান পাশের হার সত্ত্বেও গন্তব্যহীন শিক্ষা
+              ব্যবস্থা বিশ্বমানচিত্রে আমাদেরকে যোগ্যদের কাতারে দাঁড়াতে দেয়নি...।
             </p>
-            <p>
-              Dolorem officia laborum, ea ullam soluta quibusdam facere
-              reprehenderit aspernatur dicta nobis voluptates labore. Corrupti
-              ea eius ipsum quo iure illum eaque sit quam, culpa facere nisi
-              soluta! Quibusdam, commodi? Ea fugiat commodi, dolor vitae ut
-              quaerat praesentium amet nisi! Quas nemo nulla facere obcaecati
-              cupiditate laudantium veniam eveniet facilis?
+
+            <p className="mt-8">
+              <strong>প্রফেসর মোহাম্মদ নূরুনবি</strong>
+              <br />
+              চেয়ারম্যান
+              <br />
+              সাবেক অধ্যক্ষ,
+              <br />
+              রামশ্রী সরকারি কলেজ, খাগড়াছড়ি
             </p>
-            <p>
-              Harum corporis omnis ut praesentium porro dolores. In saepe
-              perspiciatis sed consequatur minus odio dolorem necessitatibus,
-              iste error architecto deleniti totam voluptas quam aspernatur
-              cupiditate perferendis hic rem vel ipsam molestiae! Iure accusamus
-              minus ullam magni ab odit quas impedit architecto! Inventore, sint
-              iure quos aliquid nam necessitatibus nobis. Ipsam?
-            </p>
-            <p>
-              Incidunt, excepturi! Fugit obcaecati culpa facilis, ex temporibus
-              blanditiis quasi rem suscipit ipsum odio quod doloremque iste
-              minima numquam officia atque non iusto vero. Non est ea veritatis
-              iste molestiae! Consectetur iste recusandae, veniam ipsum
-              excepturi sunt, sint laudantium error, obcaecati dicta ut
-              distinctio laborum eius tenetur inventore nulla? Repudiandae?
-            </p>
-            <p>
-              Harum distinctio nisi vitae deserunt maxime excepturi quos.
-              Perspiciatis optio sint eum sed, quia ad sit sequi blanditiis
-              veritatis ex tempora ab corrupti rem nam perferendis nemo ducimus
-              inventore est distinctio obcaecati consequatur doloribus atque
-              maiores eveniet? Aliquid adipisci nisi minima ipsum necessitatibus
-              obcaecati sint libero nostrum, facere commodi exercitationem!
-            </p>
-          </SwiperSlide>
-        </Swiper>
+          </div>
+
+          {/* Right side image */}
+          <div className="lg:w-1/5 mt-6 lg:mt-20 lg:ml-10 flex justify-center lg:justify-end">
+            <img
+              src={ChairmanProfile}
+              alt="Chairman"
+              className="rounded-full border-4 border-green-500 w-48 h-48 lg:w-56 lg:h-56 object-cover"
+            />
+          </div>
+        </div>
       </div>
-      {/* Placeholder for additional content */}
-      <div className="w-full md:w-1/2">
-        <h4 className="text-xl font-bold mb-4">Additional Content</h4>
-        <p>
-          This section can be used for additional content or images related to
-          the Chairman's message.
-        </p>
-      </div>
-    </div>
+    </section>
   );
 };
 
