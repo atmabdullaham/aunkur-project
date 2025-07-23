8350fcc8e9ee959da806e991292068a8f8399ad3
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-07-23 10:00:11 +0600

    added check user is admin or not functionality

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -13,7 +13,6 @@ const useAxiosSecure = () => {
   axiosSecure.interceptors.request.use(
     function (config) {
       const token = localStorage.getItem("access-token");
-      console.log("Request made with ", config);
       config.headers.authorization = `Bearer ${token}`;
       return config;
     },
